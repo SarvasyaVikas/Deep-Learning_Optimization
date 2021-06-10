352e7ee414dbbacd623b117611d7d020f47c6370
SarvasyaVikas <85037039+SarvasyaVikas@users.noreply.github.com>
2021-06-10 15:17:37 -0400

    Model_Regularization_SV- Description
    This program takes in a dataset and trains a model using Stochastic Gradient Descent. First, the model does not use regularization in training. Then, the model applies L1 and then L2 regularization before displaying the results of each type of training to the console.

--- Model_Regularization_SV.py
+++ Model_Regularization_SV.py
@@ -5,24 +5,7 @@ from sklearn.model_selection import train_test_split
 from pyimagesearch.preprocessing import SimplePreprocessor
 from pyimagesearch.datasets import SimpleDatasetLoader
 from imutils import paths
-import numpy as np
 import argparse
-import imutils
-import cv2
-import os
-
-def extract_color_histogram(image, bins=(8, 8, 8)):
-	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	hist = cv2.calcHist([hsv], [0, 1, 2], None, bins,
-		[0, 180, 0, 256, 0, 256])
-
-	if imutils.is_cv2():
-		hist = cv2.normalize(hist)
-
-	else:
-		cv2.normalize(hist, hist)
-
-	return hist.flatten()
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True)
